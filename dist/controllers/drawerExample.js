151eb62ebc7637c66c27803a7cd243de349badef
asaf eduardo lopez govea <lguaperrimo@gmail.com>
2013-10-03 12:26:40 -0500

    creada pag tips y notas
    se borró el div cerrado que estaba extra

--- dist/controllers/drawerExample.js
+++ dist/controllers/drawerExample.js
@@ -1,4 +1,22 @@
- // Listen for window.postMessage() messages that drawer sends us
+  // Listen for window.postMessage() messages that drawer sends us
+
+  document.addEventListener("deviceready", onDiviceReady, false);
+
+  function onDiviceReady(){
+    if(window.localStorage.getItem("init") != null){
+      alert("Redirecciona a la pantalla principal");
+    }
+
+    var saveBtn = document.querySelector("#save-btn");
+    saveBtn.addEventListener("click", function(){
+      window.localStorage.setItem("novio", document.querySelector("#novio"));
+      window.localStorage.setItem("novia", document.querySelector("#novia"));
+      window.localStorage.setItem("lugar", document.querySelector("#lugar"));
+      window.localStorage.setItem("fecha", document.querySelector("#fecha"));
+
+      window.localStorage.setItem("init", 1);
+    });
+  }
 
   window.addEventListener("message", function(msg) {
     var elem = document.querySelector("#selectedInDrawer");
@@ -7,7 +25,7 @@
 
   // Initialize the left drawer
 
-  var leftDrawer = new steroids.views.WebView("../views/drawerExample/drawer.html");
+  var leftDrawer = new steroids.views.WebView("/views/drawerExample/drawer.html");
 
   leftDrawer.preload({},{
     onSuccess: initGesture  // When the view has loaded, enable finger tracking
@@ -24,4 +42,4 @@
     steroids.drawers.show(leftDrawer);
   }
 
- steroids.view.navigationBar.show("Bride2Be");
+  steroids.view.navigationBar.show("Bride2Be");
