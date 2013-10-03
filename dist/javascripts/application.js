67e4b0da2f4a33ba1518af1d44fe08997ddb136b
Luis Carlos González Hernández <kentverger@kentverger-Vostro-V131.(none)>
2013-10-03 12:15:14 -0500

    Añadi sin probar el alamcenamiento de la configuracion

--- dist/javascripts/application.js
+++ dist/javascripts/application.js
@@ -1,6 +1,6 @@
 
 steroids.view.navigationBar.show("Bride2Be");
-var backButton = new steroids.buttons.NavigationBarButton();
+var backButton = new steroids.buttons.NavigationBarButton();cordova
 steroids.view.navigationBar.setButtons({
 	right: [backButton]
 });
